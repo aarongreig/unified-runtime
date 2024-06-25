15b8ce2452373f76b4e81779b37f56c7fda06bea
Aaron Greig <aaron.greig@codeplay.com>
2024-06-25 10:19:35 +0100

    Fix CI problems.

--- source/adapters/hip/device.cpp
+++ source/adapters/hip/device.cpp
@@ -881,8 +881,6 @@ UR_APIEXPORT ur_result_t UR_APICALL urDeviceGetInfo(ur_device_handle_t hDevice,
     return ReturnValue(false);
   case UR_DEVICE_INFO_TIMESTAMP_RECORDING_SUPPORT_EXP:
     return ReturnValue(true);
-  case UR_DEVICE_INFO_KERNEL_SET_SPECIALIZATION_CONSTANTS:
-    return ReturnValue(false);
 
   case UR_DEVICE_INFO_GLOBAL_VARIABLE_SUPPORT:
     return ReturnValue(false);
