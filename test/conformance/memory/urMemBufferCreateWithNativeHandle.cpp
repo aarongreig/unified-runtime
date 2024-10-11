a2dab68a5029e277713dc06d63f57063551cd656
Aaron Greig <aaron.greig@codeplay.com>
2024-10-11 16:48:20 +0100

    Update cts tests.

--- test/conformance/memory/urMemBufferCreateWithNativeHandle.cpp
+++ test/conformance/memory/urMemBufferCreateWithNativeHandle.cpp
@@ -11,7 +11,7 @@ UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urMemBufferCreateWithNativeHandleTest);
 
 TEST_P(urMemBufferCreateWithNativeHandleTest, Success) {
     ur_native_handle_t hNativeMem = 0;
-    ASSERT_SUCCESS(urMemGetNativeHandle(buffer, device, &hNativeMem));
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     // We cannot assume anything about a native_handle, not even if it's
     // `nullptr` since this could be a valid representation within a backend.
@@ -29,25 +29,28 @@ TEST_P(urMemBufferCreateWithNativeHandleTest, Success) {
     ASSERT_SUCCESS(urMemRelease(mem));
 }
 
-TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithOwnedNativeHandle) {
-    ur_native_handle_t native_handle = 0;
-    ASSERT_SUCCESS(urMemGetNativeHandle(buffer, device, &native_handle));
+TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithProperties) {
+    ur_native_handle_t hNativeMem = 0;
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     ur_mem_handle_t mem = nullptr;
+    // We can't pass isNativeHandleOwned = true in the generic tests since
+    // we always get the native handle from a UR object, and transferring
+    // ownership from one UR object to another isn't allowed.
     ur_mem_native_properties_t props = {
         /*.stype =*/UR_STRUCTURE_TYPE_MEM_NATIVE_PROPERTIES,
         /*.pNext =*/nullptr,
-        /*.isNativeHandleOwned =*/true,
+        /*.isNativeHandleOwned =*/false,
     };
-    ASSERT_SUCCESS(urMemBufferCreateWithNativeHandle(native_handle, context,
-                                                     &props, &mem));
-    ASSERT_NE(nullptr, mem);
+    ASSERT_SUCCESS(
+        urMemBufferCreateWithNativeHandle(hNativeMem, context, &props, &mem));
+    ASSERT_NE(mem, nullptr);
 
-    ur_context_handle_t mem_context = nullptr;
-    ASSERT_SUCCESS(urMemGetInfo(mem, UR_MEM_INFO_CONTEXT,
-                                sizeof(ur_context_handle_t), &mem_context,
-                                nullptr));
-    ASSERT_EQ(context, mem_context);
+    size_t alloc_size = 0;
+    ASSERT_SUCCESS(urMemGetInfo(mem, UR_MEM_INFO_SIZE, sizeof(size_t),
+                                &alloc_size, nullptr));
+
+    ASSERT_SUCCESS(urMemRelease(mem));
 }
 
 TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithUnOwnedNativeHandle) {
