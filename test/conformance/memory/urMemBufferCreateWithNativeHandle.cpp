0ba9457748e37181e76455b53d0e8e2e53ca980a
Aaron Greig <aaron.greig@codeplay.com>
2024-11-08 17:07:53 +0000

    Fix CTS tests.

--- test/conformance/memory/urMemBufferCreateWithNativeHandle.cpp
+++ test/conformance/memory/urMemBufferCreateWithNativeHandle.cpp
@@ -11,7 +11,8 @@ UUR_INSTANTIATE_DEVICE_TEST_SUITE_P(urMemBufferCreateWithNativeHandleTest);
 
 TEST_P(urMemBufferCreateWithNativeHandleTest, Success) {
     ur_native_handle_t hNativeMem = 0;
-    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(urMemGetNativeHandle(buffer, device, &hNativeMem));
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(
+        urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     // We cannot assume anything about a native_handle, not even if it's
     // `nullptr` since this could be a valid representation within a backend.
@@ -31,7 +32,8 @@ TEST_P(urMemBufferCreateWithNativeHandleTest, Success) {
 
 TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithProperties) {
     ur_native_handle_t hNativeMem = 0;
-    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(urMemGetNativeHandle(buffer, device, &hNativeMem));
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(
+        urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     ur_mem_handle_t mem = nullptr;
     // We can't pass isNativeHandleOwned = true in the generic tests since
@@ -53,30 +55,10 @@ TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithProperties) {
     ASSERT_SUCCESS(urMemRelease(mem));
 }
 
-TEST_P(urMemBufferCreateWithNativeHandleTest, SuccessWithUnOwnedNativeHandle) {
-    ur_native_handle_t native_handle = 0;
-    ASSERT_SUCCESS(urMemGetNativeHandle(buffer, device, &native_handle));
-
-    ur_mem_handle_t mem = nullptr;
-    ur_mem_native_properties_t props = {
-        /*.stype =*/UR_STRUCTURE_TYPE_MEM_NATIVE_PROPERTIES,
-        /*.pNext =*/nullptr,
-        /*.isNativeHandleOwned =*/false,
-    };
-    ASSERT_SUCCESS(urMemBufferCreateWithNativeHandle(native_handle, context,
-                                                     &props, &mem));
-    ASSERT_NE(nullptr, mem);
-
-    ur_context_handle_t mem_context = nullptr;
-    ASSERT_SUCCESS(urMemGetInfo(mem, UR_MEM_INFO_CONTEXT,
-                                sizeof(ur_context_handle_t), &mem_context,
-                                nullptr));
-    ASSERT_EQ(context, mem_context);
-}
-
 TEST_P(urMemBufferCreateWithNativeHandleTest, InvalidNullHandle) {
     ur_native_handle_t hNativeMem = 0;
-    ASSERT_SUCCESS(urMemGetNativeHandle(buffer, device, &hNativeMem));
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(
+        urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     ur_mem_handle_t mem = nullptr;
     ur_mem_native_properties_t props = {
@@ -91,7 +73,8 @@ TEST_P(urMemBufferCreateWithNativeHandleTest, InvalidNullHandle) {
 
 TEST_P(urMemBufferCreateWithNativeHandleTest, InvalidNullPointer) {
     ur_native_handle_t hNativeMem = 0;
-    ASSERT_SUCCESS(urMemGetNativeHandle(buffer, device, &hNativeMem));
+    UUR_ASSERT_SUCCESS_OR_UNSUPPORTED(
+        urMemGetNativeHandle(buffer, device, &hNativeMem));
 
     ur_mem_native_properties_t props = {
         /*.stype =*/UR_STRUCTURE_TYPE_MEM_NATIVE_PROPERTIES,
