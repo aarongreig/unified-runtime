21406b2f1a23e825e96fad73e85f50d552607425
Aaron Greig <aaron.greig@codeplay.com>
2024-11-19 16:56:44 +0000

    Fix a number of issues from the latest coverity scan.

--- source/adapters/opencl/common.cpp
+++ source/adapters/opencl/common.cpp
@@ -14,11 +14,19 @@ namespace cl_adapter {
 
 /* Global variables for urAdapterGetLastError() */
 thread_local int32_t ErrorMessageCode = 0;
-thread_local char ErrorMessage[MaxMessageSize];
+thread_local char ErrorMessage[MaxMessageSize]{};
 
 [[maybe_unused]] void setErrorMessage(const char *Message, int32_t ErrorCode) {
-  assert(strlen(Message) <= cl_adapter::MaxMessageSize);
-  strcpy(cl_adapter::ErrorMessage, Message);
+  assert(strlen(Message) < cl_adapter::MaxMessageSize);
+  // Copy at most MaxMessageSize - 1 bytes to ensure the resultant string is
+  // always null terminated.
+#if defined(_WIN32)
+  strncpy_s(cl_adapter::ErrorMessage, MaxMessageSize - 1, Message,
+            strlen(Message));
+#else
+  strncpy(cl_adapter::ErrorMessage, Message, MaxMessageSize - 1);
+#endif
+
   ErrorMessageCode = ErrorCode;
 }
 } // namespace cl_adapter
